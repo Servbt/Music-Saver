db73ad81da7459f048ec1cea0b96570126c7dec7
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-13 13:59:56 -0500

    major refactor post documentation read

--- index.js
+++ index.js
@@ -5,37 +5,46 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-let vidArray = [];
+// let vidArray = [];
+const apiKey = 'AIzaSyDxZy9vQXRU43KQCCmR2MgJHrF9bsAvyUE';
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-// route for displaying data parsed in the post request below
-app.get("/results", (req, res) => {
-    res.render("index.ejs", { videos: vidArray })
-})
+// // route for displaying data parsed in the post request below
+// app.get("/results", (req, res) => {
+//     res.render("index.ejs", { videos: vidArray })
+// })
 
-app.post("/search", (req, res) => {
-    // passing data into searchFunc.ejs to make API request
-    let videoSrch = req.body;
-    console.log(videoSrch);
+app.post("/search", async (req, res) => {
+    let videoSrch = req.body.video;
+    console.log(videoSrch)
+    try {
+        const request = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(videoSrch)}&part=snippet&type=video&maxResults=5&key=${apiKey}`);
+        res.render("index.ejs", { content: request.data.items })
+        console.log(request.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.render("index.ejs", { content: JSON.stringify(error.response.data) })
+    }
+
     res.render("index.ejs", { video: videoSrch });
 })
 
-// this post route is for preparing the data for the get request above
-app.post("/results", (req, res) => {
-    let videosList = req.body.videos;
-    // check if the array is empty, if not clears for new search
-    if (vidArray.length !== 0) {
-        vidArray = []
-    }
-    // pushing data from youtube API into our own server side array
-    videosList.forEach(video => {
-        vidArray.push(video);
-    });
-    console.log(videosList);
-})
+// // this post route is for preparing the data for the get request above
+// app.post("/results", (req, res) => {
+//     let videosList = req.body.videos;
+//     // check if the array is empty, if not clears for new search
+//     if (vidArray.length !== 0) {
+//         vidArray = []
+//     }
+//     // pushing data from youtube API into our own server side array
+//     videosList.forEach(video => {
+//         vidArray.push(video);
+//     });
+//     console.log(videosList);
+// })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
