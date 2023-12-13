cfdf9458e6f3557a3a16c0228f785fb672deece7
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-13 01:04:50 -0500

    API request info display on new route

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+let vidArray = [];
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+})
+
+app.get("/results", (req, res) => {
+    res.render("index.ejs", { videos: vidArray })
 })
 
 app.post("/search", (req, res) => {
@@ -17,10 +22,15 @@ app.post("/search", (req, res) => {
     res.render("index.ejs", { video: videoSrch });
 })
 
-app.post("/results", (req,res)=>{
-    let videosList = req.body.videos
+app.post("/results", (req, res) => {
+    let videosList = req.body.videos;
+    if (vidArray.length !== 0) {
+        vidArray = []
+    }
+    videosList.forEach(video => {
+        vidArray.push(video);
+    });
     console.log(videosList);
-    // res.render("resultsPage.ejs", {videoResults: videosList})
 })
 
 app.listen(port, () => {
