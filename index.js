70432925f17e76f27b479b6d534ab5059ed5d50f
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-14 23:41:57 -0500

    playlist deletion init and localPlaylist videofind Functionality

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const apiKey = 'AIzaSyDxZy9vQXRU43KQCCmR2MgJHrF9bsAvyUE';
 
 
-// Gloval Variable made for persistence
+// Gloval Variable made for session persistence
 let videoTarget;
 // Playlist initialization and Video Constructor
 let clientPlaylist = [];
@@ -24,10 +24,6 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-// app.get("/saved-playlist", (req, res) => {
-//     res.render("index.ejs");
-// })
-
 // Actual route used to render a single video's data, the data itself comes from the /video post route below
 app.get("/video", (req, res) => {
     res.render("index.ejs", { video: videoTarget })
@@ -36,7 +32,7 @@ app.get("/video", (req, res) => {
 // Route for viewing all items in users playlist, takes data from created playlist above
 app.get("/playlist", (req, res) => {
     let playlistString = JSON.stringify(clientPlaylist);
-    console.log(playlistString);
+    // console.log(playlistString);
     res.render("index.ejs", { playlist: clientPlaylist, playlistString: playlistString })
 })
 
@@ -55,6 +51,7 @@ app.post("/search", async (req, res) => {
 // Route from results.ejs file, once a video is clicked a API request for that singular videos info is made and used to render the page
 app.post("/video", async (req, res) => {
     let videoId = req.body.video;
+    console.log(videoId);
     try {
         const request = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet,player`);
         videoTarget = request.data.items[0]
@@ -71,6 +68,12 @@ app.post("/add-video", (req, res) => {
     let videoSent = req.body
     let video = new myVideo(videoSent.videoTitle, videoSent.videoID, videoSent.videoHtml, videoSent.videoThumbnail);
     clientPlaylist.push(video);
+})
+
+app.post("/remove", (req,res)=>{
+    let videoRm = req.body.video;
+    clientPlaylist.splice(videoRm, 1);
+    console.log(clientPlaylist);
 })
 
 app.listen(port, () => {
