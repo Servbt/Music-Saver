4d2ca7ac13767ca891680421c08c98189878b5e8
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2023-12-26 23:11:32 -0500

    env port change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+process.env.PORT = process.env.PORT || 3001;
 
 
 // Gloval Variable made for session persistence
